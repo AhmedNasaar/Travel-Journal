7fd2608858b95720be3bfcf3b56e46dfa595d5ad
NASSARYMAN <nassar@skiff.com>
2025-05-11 18:17:29 +0300

    Update Data

--- data.js
+++ data.js
@@ -1,38 +1,50 @@
 export default [
-    {
-        id: 1,
-        img: {
-            src: "https://scrimba.com/links/travel-journal-japan-image-url",
-            alt: "Mount Fuji"
-        },
-        title: "Mount Fuji",
-        country: "Japan",
-        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
-        dates: "12 Jan, 2021 - 24 Jan, 2021",
-        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+  {
+    id: 1,
+    img: {
+      src: "https://scrimba.com/links/travel-journal-japan-image-url",
+      alt: "Mount Fuji",
     },
-    {
-        id: 2,
-        img: {
-            src: "https://scrimba.com/links/travel-journal-australia-image-url",
-            alt: "Sydney Opera House"
-        },
-        title: "Sydney Opera House",
-        country: "Australia",
-        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
-        dates: "27 May, 2021 - 8 Jun, 2021",
-        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+    title: "Mount Fuji",
+    country: "Japan",
+    googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+    dates: "12 Jan, 2021 - 24 Jan, 2021",
+    text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+  },
+  {
+    id: 2,
+    img: {
+      src: "https://scrimba.com/links/travel-journal-australia-image-url",
+      alt: "Sydney Opera House",
     },
-    {
-        id: 3,
-        img: {
-            src: "https://scrimba.com/links/travel-journal-norway-image-url",
-            alt: "Geirangerfjord"
-        },
-        title: "Geirangerfjord",
-        country: "Norway",
-        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
-        dates: "01 Oct, 2021 - 18 Nov, 2021",
-        text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+    title: "Sydney Opera House",
+    country: "Australia",
+    googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+    dates: "27 May, 2021 - 8 Jun, 2021",
+    text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+  },
+  {
+    id: 3,
+    img: {
+      src: "https://scrimba.com/links/travel-journal-norway-image-url",
+      alt: "Geirangerfjord",
     },
-]
+    title: "Geirangerfjord",
+    country: "Norway",
+    googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+    dates: "01 Oct, 2021 - 18 Nov, 2021",
+    text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+  },
+  {
+    id: 4,
+    img: {
+      src: "https://mlrhpz8jmuut.i.optimole.com/cb:lD8R.5019e/w:auto/h:auto/q:mauto/f:best/ig:avif/id:5b492f4b121cabc886019aa27deaa23a/https://www.egypttoursplus.com/how-much-to-visit-pyramids-in-egypt.jpg",
+      alt: "pyramids image",
+    },
+    title: "pyramids of Giza",
+    country: "Egypt",
+    googleMapsLink: "https://maps.app.goo.gl/XUZA47HZ1aL3AgtA6",
+    dates: "27 May, 2021 - 8 Jun, 2021",
+    text: "The Great Pyramid of Giza was the tomb of pharaoh Khufu, and still contains his granite sarcophagus. It had, like other tombs of Egyptian elites, four main purposes.",
+  },
+];
